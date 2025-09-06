79fca460b6381df74ae87667ae015271236ef803
unknown <abrtapadar@gmail.com>
2025-09-06 21:55:25 +0530

    Completed code, only JSON output formatting remaining

--- TaskB.py
+++ TaskB.py
@@ -1,12 +1,13 @@
 import requests
+
 type_response=requests.get("https://pokeapi.co/api/v2/type/?name").json() #dictionary, to be filtered by keys
 types=[]
 for x in type_response["results"]: #each x in the list type_response["results"] is a dictionary with keys "name", "url"
-    types.append(x["name"])
-#types=['normal', 'fighting', 'flying', 'poison', 'ground', 'rock', 'bug', 'ghost', 'steel', 'fire', 'water', 'grass', 'electric', 'psychic', 'ice', 'dragon', 'dark', 'fairy', 'stellar', 'unknown']
+    types.append(x["name"].capitalize())
+#types=['Normal', 'Fighting', 'Flying', 'Poison', 'Ground', 'Rock', 'Bug', 'Ghost', 'Steel', 'Fire', 'Water', 'Grass', 'Electric', 'Psychic', 'Ice', 'Dragon', 'Dark', 'Fairy', 'Stellar', 'Unknown']
 #can ignore "unknown" and "stellar" types, so
 del types[-2:]
-#types=['normal', 'fighting', 'flying', 'poison', 'ground', 'rock', 'bug', 'ghost', 'steel', 'fire', 'water', 'grass', 'electric', 'psychic', 'ice', 'dragon', 'dark', 'fairy']
+#types=['Normal', 'Fighting', 'Flying', 'Poison', 'Ground', 'Rock', 'Bug', 'Ghost', 'Steel', 'Fire', 'Water', 'Grass', 'Electric', 'Psychic', 'Ice', 'Dragon', 'Dark', 'Fairy']
 #'normal' will be the first row from the bottom (as defender) then move up with other types next in index, and the first column from the left (as attacker) then move right with other types next in index
 #immunity: type takes 0 damage from another type, denoted by '0'
 #resistance: type takes half damage from another type, denoted by '0.5'
@@ -22,22 +23,22 @@ for i in range (len(types)):
     data=requests.get(f"https://pokeapi.co/api/v2/type/{x}").json()
     if data["damage_relations"]["no_damage_from"]:
         for ele in data["damage_relations"]["no_damage_from"]:
-            entry.append(ele["name"])
+            entry.append(ele["name"].capitalize())
         type_immunity[i]=entry
         entry=[]
     if data["damage_relations"]["half_damage_from"]:
         for ele in data["damage_relations"]["half_damage_from"]:
-            entry.append(ele["name"])
+            entry.append(ele["name"].capitalize())
         type_resistance[i]=entry
         entry=[]
     if data["damage_relations"]["double_damage_from"]:
         for ele in data["damage_relations"]["double_damage_from"]:
-            entry.append(ele["name"])
+            entry.append(ele["name"].capitalize())
         type_weakness[i]=entry
         entry=[]
-#type_immunity=[['ghost'], '', ['ground'], '', ['electric'], '', '', ['normal', 'fighting'], ['poison'], '', '', '', '', '', '', '', ['psychic'], ['dragon']]
-#type_resistance=['', ['rock', 'bug', 'dark'], ['fighting', 'bug', 'grass'], ['fighting', 'poison', 'bug', 'grass', 'fairy'], ['poison', 'rock'], ['normal', 'flying', 'poison', 'fire'], ['fighting', 'ground', 'grass'], ['poison', 'bug'], ['normal', 'flying', 'rock', 'bug', 'steel', 'grass', 'psychic', 'ice', 'dragon', 'fairy'], ['bug', 'steel', 'fire', 'grass', 'ice', 'fairy'], ['steel', 'fire', 'water', 'ice'], ['ground', 'water', 'grass', 'electric'], ['flying', 'steel', 'electric'], ['fighting', 'psychic'], ['ice'], ['fire', 'water', 'grass', 'electric'], ['ghost', 'dark'], ['fighting', 'bug', 'dark']]
-#type_weakness=[['fighting'], ['flying', 'psychic', 'fairy'], ['rock', 'electric', 'ice'], ['ground', 'psychic'], ['water', 'grass', 'ice'], ['fighting', 'ground', 'steel', 'water', 'grass'], ['flying', 'rock', 'fire'], ['ghost', 'dark'], ['fighting', 'ground', 'fire'], ['ground', 'rock', 'water'], ['grass', 'electric'], ['flying', 'poison', 'bug', 'fire', 'ice'], ['ground'], ['bug', 'ghost', 'dark'], ['fighting', 'rock', 'steel', 'fire'], ['ice', 'dragon', 'fairy'], ['fighting', 'bug', 'fairy'], ['poison', 'steel']]
+#type_immunity=[['Ghost'], '', ['Ground'], '', ['Electric'], '', '', ['Normal', 'Fighting'], ['Poison'], '', '', '', '', '', '', '', ['Psychic'], ['Dragon']]
+#type_resistance=['', ['Rock', 'Bug', 'Dark'], ['Fighting', 'Bug', 'Grass'], ['Fighting', 'Poison', 'Bug', 'Grass', 'Fairy'], ['Poison', 'Rock'], ['Normal', 'Flying', 'Poison', 'Fire'], ['Fighting', 'Ground', 'Grass'], ['Poison', 'Bug'], ['Normal', 'Flying', 'Rock', 'Bug', 'Steel', 'Grass', 'Psychic', 'Ice', 'Dragon', 'Fairy'], ['Bug', 'Steel', 'Fire', 'Grass', 'Ice', 'Fairy'], ['Steel', 'Fire', 'Water', 'Ice'], ['Ground', 'Water', 'Grass', 'Electric'], ['Flying', 'Steel', 'Electric'], ['Fighting', 'Psychic'], ['Ice'], ['Fire', 'Water', 'Grass', 'Electric'], ['Ghost', 'Dark'], ['Fighting', 'Bug', 'Dark']]
+#type_weakness=[['Fighting'], ['Flying', 'Psychic', 'Fairy'], ['Rock', 'Electric', 'Ice'], ['Ground', 'Psychic'], ['Water', 'Grass', 'Ice'], ['Fighting', 'Ground', 'Steel', 'Water', 'Grass'], ['Flying', 'Rock', 'Fire'], ['Ghost', 'Dark'], ['Fighting', 'Ground', 'Fire'], ['Ground', 'Rock', 'Water'], ['Grass', 'Electric'], ['Flying', 'Poison', 'Bug', 'Fire', 'Ice'], ['Ground'], ['Bug', 'Ghost', 'Dark'], ['Fighting', 'Rock', 'Steel', 'Fire'], ['Ice', 'Dragon', 'Fairy'], ['Fighting', 'Bug', 'Fairy'], ['Poison', 'Steel']]
 for i in range (len(types)):
     if type_immunity[i]!='':
         for ele in type_immunity[i]:
@@ -56,3 +57,45 @@ for i in range(len(types)):
 print("      ",end='')
 for i in range(len(types)):
     print(types[i][0:4],end=' ')
+print()
+from http.server import HTTPServer, BaseHTTPRequestHandler
+from urllib.parse import urlparse, parse_qs
+class handler(BaseHTTPRequestHandler):
+    def do_GET(self):
+        try:
+            self.send_response(200)
+            self.send_header("Content-type","text/plain")
+            self.end_headers()
+            query=urlparse(self.path).query
+            params=parse_qs(query)
+            if len(params)==1:
+                identity=list((params.keys()))[0]
+                if identity=="attacker":
+                    type_input=params["attacker"][0]
+                    n=types.index(type_input)
+                    x="["
+                    for i in range (len(arr)-1,-1,-1):
+                        x+=f"{arr[i][n]}, "
+                    x=x[:-2]+"]"
+                    self.wfile.write(bytes(x,"utf8"))
+                elif identity=="defender":
+                    type_input=params["defender"][0]
+                    x=str(arr[len(arr)-1-(types.index(type_input))])
+                    self.wfile.write(bytes(x,"utf8"))
+            elif len(params)==2:
+                att_type_input=def_type_input=0
+                for ele in params:
+                    if ele=="attacker":
+                        att_type_input=params["attacker"][0]
+                    elif ele=="defender":
+                        def_type_input=params["defender"][0]
+                row=arr[len(arr)-1-(types.index(def_type_input))]
+                entry=str(row[types.index(att_type_input)])
+                self.wfile.write(bytes(entry,"utf8"))
+        except (BrokenPipeError,ConnectionAbortedError) as err:
+                print(f"Connection aborted by client: {err}")
+        except Exception as err:
+                print(f"An unexpected error occurred: {err}")
+with HTTPServer(('', 8000),handler) as server:
+    print("Server now running!")
+    server.serve_forever()
